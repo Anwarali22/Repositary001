cfc4536338f2d4fe31eda69c88cdcb94ceb3a8e2
Anwarali22 <87905667+Anwarali22@users.noreply.github.com>
2023-08-29 15:32:42 +0530

    Delete CQRSpattern directory

--- CQRSpattern/CQRSpattern/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CQRSpattern.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
